e42be56c8e1cd07b5eca4260c748edac3f3e88ab
Demian <53890358+demian35@users.noreply.github.com>
2022-10-29 20:47:35 -0500

    Agregue funcionalidad para que se generara la comida de manera aleatoria en el tablero

--- Snake/src/PaqueteJuego/PanelSnake.java
+++ Snake/src/PaqueteJuego/PanelSnake.java
@@ -24,7 +24,7 @@ public class PanelSnake extends JPanel{
    private int cantidad; //cantidad de cuadritos
    private int res;
    private List<int[]> serpiente= new ArrayList<>();//lista de arreglos de enteros en donde guardaremos las coordenadas por donde recorrera la snake
-   private int[] comida; //arreglo donde se guardara una coordenada para imprimir la comida
+   private int[] comida =new int[2]; //arreglo donde se guardara una coordenada para imprimir la comida
    private String direccion="der"; //variable para indicar la direccion por defecto va a la derecha
    
    
@@ -39,6 +39,8 @@ public class PanelSnake extends JPanel{
        int[] b={cantidad/2,cantidad/2-1};//Par ordenado que marcara la comida
        serpiente.add(a);
        serpiente.add(b);
+       generaComida();
+       
    }
    
    //metodo paint servira para ir pintando y renderisando la imagen mientras la 
@@ -47,9 +49,14 @@ public class PanelSnake extends JPanel{
    public void paint(Graphics pintor){
        super.paint(pintor);//borramos lo pintado previamente y lo volvemos a pintar
        pintor.setColor(colorSerpiente); //asignamos el color a la serpiente
-       for(int i=0;i<serpiente.size();i++){//for para pintar cada arreglito que conformara nuestra serpiente
+       //pintando la serpiente
+       for(int i=0;i<serpiente.size();i++){//for para pintar cada celda que conformara nuestra serpiente
           pintor.fillRect(res/2+serpiente.get(i)[0]*tam,res/2+serpiente.get(i)[1]*tam,tam-1,tam-1);
-       }   
+       }
+       
+       //pintando la comida
+       pintor.setColor(puntoComida);
+       pintor.fillRect(res/2+comida[0]*tam,res/2+comida[1]*tam,tam-1,tam-1);
    }
    public void avance(){ //metodo para que la snake avance
        int [] ultimo=serpiente.get(serpiente.size()-1); //este capturara la cordenadas de cada movimiento del ultimo cuadro(cabeza)
@@ -66,9 +73,28 @@ public class PanelSnake extends JPanel{
            break;
        }
        
-       int[] nuevaposicion={ultimo[0]+agregarx,ultimo[1]+agregary}; //es la nueva posicion que tendra el ultimo cuadrito(cabeza)
+       int[] nuevaposicion={Math.floorMod(ultimo[0]+agregarx,cantidad),
+           Math.floorMod(ultimo[1]+agregary,cantidad)}; //es la nueva posicion que tendra el ultimo cuadrito(cabeza)
        serpiente.add(nuevaposicion); //pintamos el nuevo cuadrito para reflejar el movimiento
        serpiente.remove(ultimo);//borramos el cuadrito previo
+   }
+   
+   public void generaComida(){//funcion que generara comida buscando coordenadas que no esten ocupadas por la serpiente de forma aleatoria
+       boolean existe=false; //booleano que determinara si una celda o coordenada  ya esta ocupada o no
+       int a= (int)(Math.random()*cantidad); //buscara en el eje de las x, Se castea porque se necesitan valores enteros forzosamente
+       int b=(int)(Math.random()*cantidad);
+       for(int i=0;i<serpiente.size();i++){ //recorremos la serpiente para ver cuales celdas estan ocupadas por la misma
+           if(a==0 && b==1){
+               existe=true;
+               generaComida();
+               break;
+           }
+       }
+        
+       if(!existe){
+           this.comida[0]=a;
+           this.comida[1]=b;
+       }
    }
 }
 
